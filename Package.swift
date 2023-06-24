11970b326994c976e81f92357f63a986cec84761
Mudasir Ahmed <mudasir.ahmed@united.com>
2023-06-24 01:59:51 -0500

    [Mudasir] updated

--- Package.swift
+++ Package.swift
@@ -1,23 +1,31 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
+
     name: "FavOnboardingKit",
+    platforms: [
+        .iOS(.v13)
+    ],
+
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FavOnboardingKit",
             targets: ["FavOnboardingKit"]),
     ],
+    
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0.0")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FavOnboardingKit"),
+            name: "FavOnboardingKit",
+            dependencies: ["SnapKit"]
+        ),
         .testTarget(
             name: "FavOnboardingKitTests",
-            dependencies: ["FavOnboardingKit"]),
+            dependencies: ["FavOnboardingKit"]
+        ),
     ]
 )
